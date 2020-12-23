046554ae216fc3466a932a9ee7f26e683988250a
Danil Kleshchin <danil.kleshchin@gmail.com>
2020-12-23 13:33:32 +0300

    Update Feed entity

--- domain/src/main/java/com/danil/kleshchin/rss/domain/entity/Feed.kt
+++ domain/src/main/java/com/danil/kleshchin/rss/domain/entity/Feed.kt
@@ -1,4 +1,9 @@
 package com.danil.kleshchin.rss.domain.entity
 
-class Feed {
-}
+data class Feed(
+    val title: String,
+    val description: String,
+    val fulFeedPageUrl: String,
+    val date: String,
+    val iconUrl: String
+)
