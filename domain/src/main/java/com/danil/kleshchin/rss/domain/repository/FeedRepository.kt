e729d737c2c054e3ec579f53dd4f5ceb743462fb
Danil Kleshchin <danil.kleshchin@gmail.com>
2020-12-26 22:43:49 +0300

    Make FeedRepository public

--- domain/src/main/java/com/danil/kleshchin/rss/domain/repository/FeedRepository.kt
+++ domain/src/main/java/com/danil/kleshchin/rss/domain/repository/FeedRepository.kt
@@ -3,7 +3,7 @@ package com.danil.kleshchin.rss.domain.repository
 import com.danil.kleshchin.rss.domain.entity.Feed
 import io.reactivex.Observable
 
-interface FeedRepository {
+public interface FeedRepository {
 
     fun getFeedListBySection(sectionId: Int): Observable<List<Feed>>
 }
