a01287896533f8b8f0ea89ac4ed7452487b76d86
Danil Kleshchin <danil.kleshchin@gmail.com>
2020-12-26 23:22:21 +0300

    Make UseCase params nullable

--- domain/src/main/java/com/danil/kleshchin/rss/domain/interactor/feed/GetFeedBySectionUseCase.kt
+++ domain/src/main/java/com/danil/kleshchin/rss/domain/interactor/feed/GetFeedBySectionUseCase.kt
@@ -3,13 +3,14 @@ package com.danil.kleshchin.rss.domain.interactor.feed
 import com.danil.kleshchin.rss.domain.entity.Feed
 import com.danil.kleshchin.rss.domain.interactor.UseCase
 import com.danil.kleshchin.rss.domain.repository.FeedRepository
+import com.sun.crypto.provider.Preconditions
 import io.reactivex.Observable
 
 class GetFeedBySectionUseCase(
     private val repository: FeedRepository
 ) : UseCase<List<Feed>, GetFeedBySectionUseCase.Params> {
 
-    override fun execute(params: Params): Observable<List<Feed>> {
+    override fun execute(params: Params?): Observable<List<Feed>> {
         return repository.getFeedListBySection(params.sectionPosition)
     }
 
